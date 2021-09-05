fa2ccd65da1620495cb6e0ab82e4c501047adbde
Adhidarma Hadiwinoto <adhisimon@gmail.com>
2021-09-05 17:12:18 +0700

    Add yargs usage param

--- index.js
+++ index.js
@@ -5,10 +5,12 @@ const { hideBin } = require('yargs/helpers');
 const { machineIdSync } = require('node-machine-id');
 
 const { argv } = yargs(hideBin(process.argv))
+    .usage('CLI tool to extract machine-id. Compatible with Tektrans License Manager.')
     .option('original', {
         type: 'boolean',
         description: 'If true return original value of machine id, otherwise return hashed value (sha-256), default: false',
-    });
+    })
+    .epilog('Copyright Tektrans 2021. http://tektrans.id/');
 
 // eslint-disable-next-line no-console
 console.log(
